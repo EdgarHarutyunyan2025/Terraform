ff89eb0a942e68f9424762a051b56f4474a167f1
edgar_harutyunyan_2025 <edgar_harutyunyan_2025@mail.ru>
2025-02-28 17:13:11 +0400

    terraform lessons

---
+++ lesson-13/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "web" {
+  name = var.security_group_name
+  dynamic "ingress" {
+    for_each = var.allow_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = var.common_tags
+}
+
